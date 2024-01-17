86cc8ffcbee302ac795fad615ed752e9f1cf39a6
Conor Furlong <cfurlong15@gmail.com>
2024-01-17 16:22:37 -0500

    update and delete project successful

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('projects/<int:project_id>/', views.projects_detail, name='detail'),
     path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
+    path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name='projects_update'),
+    path('projects/<int:pk>/delete/', views.ProjectDelete.as_view(), name='projects_delete'),
 ]
