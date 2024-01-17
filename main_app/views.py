529911ddbd4a4c17d6c02f48f69a175ef792810f
Conor Furlong <cfurlong15@gmail.com>
2024-01-16 23:21:08 -0500

    create profile model

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+# from .forms import ProfileForm
 
 # Create your views here.
 
@@ -18,24 +20,45 @@ def projects_index(request):
         'projects': projects
     })
 
+# def signup(request):
+#     error_message = ''
+#     if request.method == 'POST':
+#     # This is how to create a 'user' form object
+#     # that includes the data from the browser
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             # This adds the user to the database
+#             user = form.save()
+#             # This is how we log a user in
+#             login(request, user)
+#             return redirect('index')
+#         else:
+#             error_message = 'Invalid sign up - try again'
+#             # A bad POST or GET request, so render signup.html with empty form
+#     # form = UserCreationForm()
+#     form = UserCreationForm()
+#     context = {'form': form, 'error_message': error_message}
+#     return render(request, 'registration/signup.html', context)
+
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-        form = UserCreationForm(request.POST)
+        # Use the custom SignUpForm that includes first name and last name fields
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            # This adds the user to the database
             user = form.save()
-            # This is how we log a user in
+            user.refresh_from_db()  # Load the profile instance created by the signal
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.save()
             login(request, user)
             return redirect('index')
         else:
             error_message = 'Invalid sign up - try again'
-            # A bad POST or GET request, so render signup.html with empty form
-    form = UserCreationForm()
+    else:
+        # Use the custom SignUpForm for GET requests
+        form = SignUpForm()
+
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
-
-# def logout_view(request):
-#     logout(request)
