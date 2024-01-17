efc72501c9697c7705471da51ba19c6394f46eac
Conor Furlong <cfurlong15@gmail.com>
2024-01-17 11:17:49 -0500

    add project model

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+STATUS = (
+    ('not_started', 'Not Started'),
+    ('in_progress', 'In Progress'),
+    ('reviewing', 'Reviewing'),
+    ('on_hold', 'On Hold'),
+    ('completed', 'Completed')
+)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
@@ -19,3 +27,17 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    start_date = models.DateField('start date')
+    end_date = models.DateField('end date')
+    status = models.CharField(
+        max_length=11,
+        choices=STATUS,
+        default=STATUS[0][0]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- main_app/views.py
+++ main_app/views.py
@@ -6,19 +6,16 @@ from .forms import SignUpForm
 
 # Create your views here.
 
-projects = [
-    {'name': 'Project 4', 'start_date': '1/12/2024', 'end_date': '1/22/2024', 'description': 'Create project management app and successfully graduate.'},
-    {'name': 'Portfolio', 'start_date': '1/1/2024', 'end_date': '1/23/2024', 'description': 'Build an amazing portfolio to show off all your great work.'},
-
-]
+# projects = [
+#     {'name': 'Project 4', 'start_date': '1/12/2024', 'end_date': '1/22/2024', 'description': 'Create project management app and successfully graduate.'},
+#     {'name': 'Portfolio', 'start_date': '1/1/2024', 'end_date': '1/23/2024', 'description': 'Build an amazing portfolio to show off all your great work.'},
+# ]
 
 def home(request):
     return render(request, 'home.html')
 
 def projects_index(request):
-    return render(request, 'projects/index.html', {
-        'projects': projects
-    })
+    return render(request, 'projects/index.html')
 
 # def signup(request):
 #     error_message = ''
