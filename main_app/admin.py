1fc45d857b5471364c2fe30e36623ae7a19862b2
Conor Furlong <cfurlong15@gmail.com>
2024-01-17 20:41:09 -0500

    create add task functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Profile
-from.models import Project
+from .models import Profile, Project, Task
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Project)
+admin.site.register(Task)
